f0f4024d38413100d917882726dce665b089dd8c
omerpeled13 <138997776+omerpeled13@users.noreply.github.com>
2025-02-20 22:52:36 +0700

    שינוי צבע טבלה ניהול קבצים

--- src/components/main/FileManager.tsx
+++ src/components/main/FileManager.tsx
@@ -388,7 +388,7 @@ export function FileManager() {
               {/* Table Header - Fixed */}
               <div className="overflow-hidden">
                 <Table className="table-fixed w-full">
-                  <TableHeader className="container sticky top-0 shadow-md z-10">
+                  <TableHeader className="sticky top-0 shadow-md z-10">
                     <TableRow>
                       <TableHead className="text-right">{'דו"ח'}</TableHead>
                       {user?.role === 'admin' && <TableHead className="text-right">לקוח</TableHead>}
